4c5f435ae13d18b462618b0ad9229d0d091e9932
Akkun716 <T1M34git_HUB>
2019-10-21 12:40:46 -0700

    Finalized diagonal check and recursive movement through board

--- NQueens.java
+++ NQueens.java
@@ -7,245 +7,69 @@ public class NQueens
 
     public NQueens (int n)
     {
-        try
+        if (n >= 0)
         {
-            if (n > 0)
-            {
-                size = n;
-                board = new int[size][size];
-                for(int row = 0; row < size; row++)
-                {
-                    for(int col = 0; col < size; col++)
-                    {
-                        board[row][col] = 0;
-                    }
-                }
-            }
+            size = n;
+            board = new int[size][size];
         }
-        catch(Exception e)
-        {
-            System.out.print("The size of the board should at least be 1...");
-        }
-    }
-
-    private int row = 0;
-    private int col = 0;
-
-    private boolean Qcheck_H(/*int row, int col*/)
-    {
-        for(int col = 0; col < size; col++)
-        {
-            if(board[row][col] == 1)
-            {
-                return false;
-            }
-        }
-        return true;
-
-//        if(board[row][col] == 1)
-//        {
-//            return false;
-//        }
-//        else if(col == size - 1)
-//        {
-//            return true;
-//        }
-//        else
-//        {
-//            return Qcheck_H(row, col++);
-//        }
-    }
-
-    private boolean Qcheck_V(/*int row, int col*/)
-    {
-        for(int row = 0; row < size; row++)
-        {
-            if(board[row][col] == 1)
-            {
-                return false;
-            }
-        }
-        return true;
-
-//        if(board[row][col] == 1)
-//        {
-//            return false;
-//        }
-//        else if(row == size - 1)
-//        {
-//            return true;
-//        }
-//        else
-//        {
-//            return Qcheck_V(row++, col);
-//        }
-    }
-
-    private boolean Qcheck_RDU(int row, int col)
-    {
-        for(int i = 0; i < size - col; i++)
-        {
-            if(board[row - i][col + i] == 1)
-            {
-                return false;
-            }
-        }
-        return true;
-
-//        if(board[row][col] == 1)
-//        {
-//            return false;
-//        }
-//        else if(col == size - 1)
-//        {
-//            return true;
-//        }
-//        else
-//        {
-//            return Qcheck_RDU(row--, col++);
-//        }
-    }
-
-    private boolean Qcheck_RDD(int row, int col)
-    {
-        for(int i = 0; i < size - col; i++)
-        {
-            if(board[row + i][col - i] == 1)
-            {
-                return false;
-            }
-        }
-        return true;
-
-//        if(board[row][col] == 1)
-//        {
-//            return false;
-//        }
-//        else if(col == 0)
-//        {
-//            return true;
-//        }
-//        else
-//        {
-//            return Qcheck_RDD(row++, col--);
-//        }
-    }
-
-    private boolean Qcheck_LDU(int row, int col)
-    {
-        for(int i = 0; i < col; i++)
-        {
-            if(board[row - i][col + i] == 1)
-            {
-                return false;
-            }
-        }
-        return true;
-
-//        if(board[row][col] == 1)
-//        {
-//            return false;
-//        }
-//        else if(col == 0)
-//        {
-//            return true;
-//        }
-//        else
-//        {
-//            return Qcheck_LDU(row--, col--);
-//        }
-    }
-
-    private boolean Qcheck_LDD(int row, int col)
-    {
-        for(int i = 0; i < col; i++)
-        {
-            if(board[row + i][col - i] == 1)
-            {
-                return false;
-            }
-        }
-        return true;
-
-//        if(board[row][col] == 1)
-//        {
-//            return false;
-//        }
-//        else if(col == size - 1)
-//        {
-//            return true;
-//        }
-//        else
-//        {
-//            return Qcheck_LDD(row++, col++);
-//        }
-    }
-
-    private boolean Qcheck_D(int row, int col)
-    {
-        return Qcheck_LDD(row, col) && Qcheck_LDU(row, col) &&
-                Qcheck_RDD(row, col) && Qcheck_RDU(row, col);
     }
 
     private boolean Qcheck(int row, int col)
     {
-        return Qcheck_H(/*row, 0*/) && Qcheck_V(/*0, col*/) && Qcheck_D(row, col);
-    }
-
-    private boolean boardFill()
-    {
-        int count = 0;
-        for(int row = 0; row < size; row++)
+        //Horizontal check
+        for(int c = 0; c < size; c++)
         {
-            for(int col = 0; col < size; col++)
+            if(board[row][c] == 1)
             {
-                if(board[row][col] == 1)
-                {
-                    count++;
-                }
+                return false;
             }
         }
 
-        if(count == size)
+        //Vertical check
+        for(int r = 0; r < size; r++)
         {
-            return true;
+            if(board[r][col] == 1)
+            {
+                return false;
+            }
         }
 
-        return false;
+        //Diagonal check
+        for (int r = row, c = col; r >= 0 && c >= 0; r--, c--) {
+            if (board[r][c] == 1){
+                return false;
+            }
+        }
+        for (int r = row, c = col; r >= 0 && c < size; r--, c++) {
+            if (board[r][c] == 1) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
-    private boolean placeNQueens(int row, int col)
+    private boolean placeNQueens(int row)
     {
-        if(boardFill())
+        if(row == size)
         {
+            printToConsole();
             return true;
         }
-        else if(row != size - 1)
+
+        for(int i = 0; i < size; i++)
         {
-            if (Qcheck(row, col))
+            if(Qcheck(row, i))
             {
-                board[row][col] = 1;
-                row++;
-                return placeNQueens(row, col);
-            }
-            else
-            {
-                if(col == size - 1)
+                board[row][i] = 1;
+
+                if(placeNQueens(row + 1))
                 {
-                    return false;
+                    return true;
                 }
                 else
                 {
-                    row--;
-                    for (int i = 0; i < size; i++)
-                    {
-                        if (board[row][i] == 1)
-                        {
-                            board[row][i] = 0;
-                        }
-                    }
-                    col++;
-                    return placeNQueens(row, col);
+                    board[row][i] = 0;
                 }
             }
         }
@@ -253,18 +77,33 @@ public class NQueens
         return false;
     }
 
-    public boolean placeNQueens()
+    public boolean placeNQueens() throws Exception
     {
-        printToConsole();
-        return placeNQueens(row, col);
+        if(size <= 0)
+        {
+            throw new Exception();
+        }
+
+        return placeNQueens(0);
     }
 
     public void printToConsole()
     {
-        String consoleBoard = "";
+        String consoleBoard = " ";
+        for(int i = 0; i < size; i++)
+        {
+            consoleBoard += "=";
+            if(i > 0)
+            {
+                consoleBoard += "==";
+            }
+        }
+
+        consoleBoard += "\n";
 
         for(int row = 0; row < size; row++)
         {
+            consoleBoard += "|";
             for(int col = 0; col < size; col++)
             {
                 if(board[row][col] == 1)
@@ -282,8 +121,20 @@ public class NQueens
                 }
             }
 
-            consoleBoard += "\n";
+            consoleBoard += "|\n";
         }
+
+        consoleBoard += " ";
+
+        for(int i = 0; i < size; i++)
+        {
+            consoleBoard += "=";
+            if(i > 0)
+            {
+                consoleBoard += "==";
+            }
+        }
+
 
         System.out.println(consoleBoard);
     }
